edb772ccc3f4f9470b583044efb052e4e17e2f66
herzaparam <herzaparam@gmail.com>
2021-05-20 19:16:10 +0700

    fix register, graph

--- src/components/module/GraphHome/index.js
+++ src/components/module/GraphHome/index.js
@@ -9,13 +9,13 @@ function Graph() {
                 <div className="graph-top-income">
                     <img src="/arrow-up-green.png" alt="" />
                     <p>Income</p>
-                    <h3>Rp.2.000.000</h3>
+                    <h3>-</h3>
                 </div>
                 <div className="graph-top-outcome">
                     <div className="graph-top-income">
                         <img src="/arrow-up-red.png" alt="" />
                         <p>Outcome</p>
-                        <h3>Rp.5.000.000</h3>
+                        <h3>-</h3>
                     </div>
                 </div>
             </div>
